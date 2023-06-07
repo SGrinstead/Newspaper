5bd0310559113d4a5cbaf48df63c80330bca69a4
SGrinstead <seth.grinstead3299@gmail.com>
2023-06-07 12:34:19 -0700

    reporter class

---
+++ Newspaper.UnitTests/ReporterTests.cs
@@ -0,0 +1,21 @@
+using System.Net;
+
+namespace Newspaper.UnitTests
+{
+    public class ReporterTests
+    {
+        [Fact]
+        public void Reporter_CanSetProperties()
+        {
+            Reporter reporter = new Reporter()
+            {
+                Name = "Lois Lane",
+                Specialty = "Politics"
+            };
+
+            Assert.Equal("Lois Lane", reporter.Name);
+            Assert.Equal("Politics", reporter.Specialty);
+            Assert.Equal(new List<Article>(), reporter.Articles);
+        }
+    }
+}
