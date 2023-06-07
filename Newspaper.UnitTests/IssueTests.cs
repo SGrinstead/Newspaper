3ef1d92aa7c3af9d3322e2b098584326352d91af
SGrinstead <seth.grinstead3299@gmail.com>
2023-06-07 14:23:18 -0700

    statistics methods with tests

--- Newspaper.UnitTests/IssueTests.cs
+++ Newspaper.UnitTests/IssueTests.cs
@@ -20,5 +20,125 @@ namespace Newspaper.UnitTests
                 , issue.Date.ToShortDateString());
             Assert.Equal(new List<Article>(), issue.Articles);
         }
+
+        [Fact]
+        public void Issue_ArticleCount_ReturnsCountOfArticles()
+        {
+            Reporter reporter = new Reporter()
+            {
+                Name = "Lois Lane",
+                Specialty = "Politics"
+            };
+            Issue issue = new Issue()
+            {
+                Date = DateTime.UtcNow
+            };
+            Article article = new Article()
+            {
+                Title = "The Superman is Real, and He's American!",
+                Content = "a bunch of words",
+                Reporter = reporter,
+                Issue = issue
+            };
+
+            issue.Articles.Add(article);
+            issue.Articles.Add(article);
+            issue.Articles.Add(article);
+
+            Assert.Equal(3, issue.ArticleCount());
+        }
+
+        [Fact]
+        public void Issue_ReporterCount_ReturnsCountOfUniqueReporters()
+        {
+            Issue issue = new Issue()
+            {
+                Date = DateTime.UtcNow
+            };
+            Reporter lois = new Reporter()
+            {
+                Name = "Lois Lane",
+                Specialty = "Politics"
+            };
+            Reporter sport = new Reporter()
+            {
+                Name = "Mr. Sportsball",
+                Specialty = "Sports"
+            };
+            Article article1 = new Article()
+            {
+                Title = "The Superman is Real, and He's American!",
+                Content = "a bunch of words",
+                Reporter = lois,
+                Issue = issue
+            };
+            Article article2 = new Article()
+            {
+                Title = "Home Team Wins Game",
+                Content = "they won.",
+                Reporter = sport,
+                Issue = issue
+            };
+            Article article3 = new Article()
+            {
+                Title = "Home Team Loses Game",
+                Content = "they lost.",
+                Reporter = sport,
+                Issue = issue
+            };
+
+            issue.Articles.Add(article1);
+            issue.Articles.Add(article2);
+            issue.Articles.Add(article3);
+
+            Assert.Equal(2, issue.ReporterCount());
+        }
+
+        [Fact]
+        public void Issue_ReporterNames_ReturnsListOfUniqueReporterNames()
+        {
+            Issue issue = new Issue()
+            {
+                Date = DateTime.UtcNow
+            };
+            Reporter lois = new Reporter()
+            {
+                Name = "Lois Lane",
+                Specialty = "Politics"
+            };
+            Reporter sport = new Reporter()
+            {
+                Name = "Mr. Sportsball",
+                Specialty = "Sports"
+            };
+            Article article1 = new Article()
+            {
+                Title = "The Superman is Real, and He's American!",
+                Content = "a bunch of words",
+                Reporter = lois,
+                Issue = issue
+            };
+            Article article2 = new Article()
+            {
+                Title = "Home Team Wins Game",
+                Content = "they won.",
+                Reporter = sport,
+                Issue = issue
+            };
+            Article article3 = new Article()
+            {
+                Title = "Home Team Loses Game",
+                Content = "they lost.",
+                Reporter = sport,
+                Issue = issue
+            };
+
+            issue.Articles.Add(article1);
+            issue.Articles.Add(article2);
+            issue.Articles.Add(article3);
+
+            List<string> expected = new List<string>() { "Lois Lane", "Mr. Sportsball" };
+            Assert.Equal(expected, issue.ReporterNames());
+        }
     }
 }
--- Newspaper/Models/Issue.cs
+++ Newspaper/Models/Issue.cs
@@ -11,5 +11,22 @@ namespace Newspaper.Models
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public List<Article> Articles { get; set; } = new();
+
+        public int ArticleCount()
+        {
+            return Articles.Count();
+        }
+
+        public int ReporterCount()
+        {
+            var reporters = Articles.Select(article => article.Reporter);
+            return reporters.Distinct().Count();
+        }
+
+        public List<string> ReporterNames()
+        {
+            var names = Articles.Select(article => article.Reporter.Name);
+            return names.Distinct().ToList();
+        }
     }
 }
